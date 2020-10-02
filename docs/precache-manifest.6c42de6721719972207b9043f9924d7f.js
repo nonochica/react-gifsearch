01c90b0cb71df5f3149738cbdf7680725f0b450e
Antonio Chica <antonio.chicajimenez@gmail.com>
2020-10-02 12:15:09 +0200

    docs agregado

---
+++ docs/precache-manifest.6c42de6721719972207b9043f9924d7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb620d175200c4536b48ed9507816473",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ae59cdfd97907189c9b",
+    "url": "/static/css/main.b25ba940.chunk.css"
+  },
+  {
+    "revision": "f282757b0c05702f74d8",
+    "url": "/static/js/2.b1f59dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1f59dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae59cdfd97907189c9b",
+    "url": "/static/js/main.c7b31722.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
